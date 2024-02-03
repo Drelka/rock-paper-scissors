49b9c498b0c0b10fd1b15aa1ba9245653b11ad68
Drelka <47503020+Drelka@users.noreply.github.com>
2024-02-03 20:28:13 +0000

    Finished rps project

--- script.js
+++ script.js
@@ -3,42 +3,54 @@ const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
 const result = document.querySelector("#result");
 const points = document.querySelector("#points");
+const endOfGame = document.querySelector("#endOfGame");
+const resetBtn = document.querySelector("#resetButton");
+const buttons3 = document.querySelectorAll(".btn");
 
 let options = ['rock', 'paper', 'scissors'];
 
+function countPoints() {
+    points.textContent = `${playerScore} : ${compScore}`;
+}
+
 let playerScore = 0;
 let compScore = 0;
+countPoints();
+result.textContent = "Choose rock, paper or scissors.";
 
 rockBtn.addEventListener("click", () => {
     result.textContent = (playRound("rock"));
-    if (x == "scissors") {
+    if (compChoice == "scissors") {
         playerScore += 1}
-    else if (x == "paper") {
+    else if (compChoice == "paper") {
         compScore += 1}
-    points.textContent = `${playerScore} : ${compScore}`;
+    countPoints();
+    endGame();
 });
 
 paperBtn.addEventListener("click", () => {
     result.textContent = (playRound("paper"));
-    if (x == "rock") {
+    if (compChoice == "rock") {
         playerScore += 1}
-    else if (x == "scissors") {
+    else if (compChoice == "scissors") {
         compScore += 1}
-    points.textContent = `${playerScore} : ${compScore}`;
+    countPoints();
+    endGame();
 });
 
 scissorsBtn.addEventListener("click", () => {
     result.textContent = (playRound("scissors"));
-    if (x == "paper") {
+    if (compChoice == "paper") {
         playerScore += 1}
-    else if (x == "rock") {
+    else if (compChoice == "rock") {
         compScore += 1}
-    points.textContent = `${playerScore} : ${compScore}`;
+    countPoints();
+    endGame();
 });
 
 
 function getComputerChoice(){
-    return x = options[Math.floor(Math.random() * options.length)];
+    return compChoice = options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection){
@@ -47,17 +59,14 @@ function playRound(playerSelection, computerSelection){
     playerSelection;
     
     let result;
-    let wrongInput = "No such choice. Try again!";
     
     if (computerSelection === "rock"){
         if (playerSelection === "rock"){
             result = "Tie!";
         } else if (playerSelection === "paper"){
             result = "Paper beats rock. You win!";
-        } else if (playerSelection === "scissors"){
-            result = "Rock beats scissors. You lose!";
         } else {
-            return wrongInput;
+            result = "Rock beats scissors. You lose!";
         }
     }
     
@@ -66,10 +75,8 @@ function playRound(playerSelection, computerSelection){
             result = "Paper beats rock. You lose!";
         } else if (playerSelection === "paper"){
             result = "Tie!";
-        } else if (playerSelection === "scissors"){
-            result = "Scissors beats paper. You win!";
         } else {
-            return wrongInput;
+            result = "Scissors beats paper. You win!";
         }
     }
     
@@ -78,10 +85,8 @@ function playRound(playerSelection, computerSelection){
             result = "Rock beats scissors. You win!";
         } else if (playerSelection === "paper"){
             result = "Scissors beats paper. You lose!";
-        } else if (playerSelection === "scissors"){
-            result = "Tie!";
         } else {
-            return wrongInput;
+            result = "Tie!";
         }
     }
 
@@ -89,4 +94,41 @@ function playRound(playerSelection, computerSelection){
     return finalResult;
 }
 
-
+function endGame(){
+    if(playerScore == 5 || compScore == 5) {
+        btn = document.createElement("button");
+        if (playerScore == 5) {
+            if((playerScore - compScore) > 1) {
+                let x = playerScore - compScore;
+                endOfGame.textContent = `YOU WON THE GAME BY ${x} POINTS!`;
+            } else {
+                endOfGame.textContent = "YOU WIN THE GAME BY 1 POINT!";
+            }
+        } else {
+            if((compScore - playerScore) > 1 ){
+                let x = compScore - playerScore;
+                endOfGame.textContent = `COMPUTER BEATS YOU BY ${x} POINTS!`;
+            } else {
+                endOfGame.textContent = "COMPUTER BEATS YOU BY 1 POINT!";
+            }
+        }
+        resetBtn.appendChild(btn);
+        buttons3.forEach(button => {
+            button.disabled = true;
+        });
+        
+        btn.style.cssText = "padding: 10px; font-size: 20px; margin-top: 50px";
+        btn.textContent = "Play Again";
+        btn.addEventListener("click", () => {
+            playerScore = 0;
+            compScore = 0;
+            countPoints();
+            result.textContent = "Choose rock, paper or scissors.";
+            endOfGame.textContent = "";
+            resetBtn.removeChild(btn);
+            buttons3.forEach(button => {
+                button.disabled = false;
+            });
+        });
+    }
+}
